cee3ac8567591c4cc32292510eea5cf7a09a2ce2
Haza Pro <hazaelfg@gmail.com>
2024-10-02 05:43:47 -0700

    Mas traducciones

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import HomeView from './components/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import SeasonDetails from './views/SeasonDetails.vue'
 import { ROUTES } from './globals'
 
 
