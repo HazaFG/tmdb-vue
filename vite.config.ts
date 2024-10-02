57f3d871eb5b8bc82472cf95b2074e90d02e44e0
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-10-01 21:55:33 -0700

    espero k jale jijijijijijij

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // DESCOMENTAR ESTO CUANDO DEPLOY PORQUE EN LOCAL T HACE COSAS LOKITAS
+  // base: `/tmdb-vue`,
 })
